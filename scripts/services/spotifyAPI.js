ea3fda598957da92acfbe7974eb49870c9332eeb
Jesse van Assen <jesse.v.assen@gmail.com>
2014-10-11 20:06:43 +0200

    Added a method to the spotifyAPI to retrieve the user's id.

--- scripts/services/spotifyAPI.js
+++ scripts/services/spotifyAPI.js
@@ -1,6 +1,22 @@
 (function() {
-	function SpotifyAPI(appSettings) {
+	function SpotifyAPI($http, appSettings) {
 		var self = this;
+		var API_ENDPOINT = "https://api.spotify.com/v1";
+
+		var getHeaders = function(authorization) {
+			if (authorization == null || !authorization.isValid())
+				return { };
+
+			return {
+				headers: {
+					Authorization: authorization.tokenType + " " + authorization.accessToken
+				}
+			};
+		}
+
+		self.loadUserId = function(authorization) {
+			return $http.get(API_ENDPOINT + "/me", getHeaders(authorization)).then(function(result) { return result.data.id; });
+		}
 	};
 
 	angular.module("spotify-playlist-deduplicator").service("spotifyAPI", SpotifyAPI);
