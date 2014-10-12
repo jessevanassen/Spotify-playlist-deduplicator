e9612957aaf35961db8c88fe4fbfe72eddfda531
Jesse van Assen <jesse.v.assen@gmail.com>
2014-10-12 18:23:11 +0200

    - Only the required fields are queried. - Querying a playlist's tracks also includes the snapshotId. - Made the API's responses consistent.

--- scripts/services/spotifyAPI.js
+++ scripts/services/spotifyAPI.js
@@ -4,54 +4,107 @@
 		var API_ENDPOINT = "https://api.spotify.com/v1";
 
 		var getHeaders = function(authorization) {
-			if (authorization === undefined || authorization === null || !authorization.isValid())
-				return { };
-
-			return {
-				headers: {
-					Authorization: authorization.tokenType + " " + authorization.accessToken
-				}
+			var headers = {
+				"Content-Type": "application/json"
 			};
+
+			if (authorization && authorization.isValid())
+				headers.Authorization = authorization.tokenType + " " + authorization.accessToken;
+
+			return headers;
 		};
 
-		var loadAll = function(url, authorization) {
+		var loadAll = function(config, authorization) {
 			var deferred = $q.defer();
 
-			var loadAll_ = function(url, accumulator) {
-				$http.get(url, getHeaders(authorization))
+			var load = function(config, accumulator) {
+				$http(config)
 					.then(function(result) {
 						accumulator = accumulator.concat(result.data.items);
-						if (result.data.next)
-							loadAll_(result.data.next, accumulator);
-						else
+						if (result.data.next) {
+							load({
+								url: result.data.next,
+								headers: config.headers
+							}, accumulator);
+						} else {
 							deferred.resolve(accumulator);
+						}
 					})
 					.catch(deferred.reject);
 			};
-			loadAll_(url, []);
+			load(config, []);
 
 			return deferred.promise;
 		};
 
 		self.loadUserId = function(authorization) {
-			return $http.get(API_ENDPOINT + "/me", getHeaders(authorization))
-				.then(function(result) { return result.data.id; });
+			return $http({
+				url: API_ENDPOINT + "/me",
+				params: {
+					fields: "id"
+				},
+				headers: getHeaders(authorization)
+			})
+				.then(function(result) {
+					return result.data.id;
+				});
 		};
 
 		self.loadPlaylists = function(userId, authorization) {
-			return loadAll(API_ENDPOINT + "/users/" + userId + "/playlists?limit=50", authorization)
+			return loadAll({
+				url: API_ENDPOINT + "/users/" + userId + "/playlists",
+				params: {
+					fields: ["next", "items.id", "items.name", "items.owner.id", "items.uri"].join(","),
+					limit: 50
+				},
+				headers: getHeaders(authorization)
+			})
 				.then(function(data) {
-					return data.map(function(playlist) { return { id: playlist.id, name: playlist.name }; });
+					return {
+						items: data
+							.filter(function(playlist) {
+								return playlist.owner.id == userId;
+							})
+							.map(function(playlist) {
+								return {
+									id: playlist.id,
+									name: playlist.name,
+									uri: playlist.uri
+								};
+							})
+					};
 				});
 		};
 
 		self.loadTracksInPlaylist = function(userId, playlistId, authorization) {
-			return loadAll(API_ENDPOINT + "/users/" + userId + "/playlists/" + playlistId + "/tracks?limit=100", authorization)
-				.then(function(data) {
-					return data.map(function(playlistItem) {
-						console.log(arguments);
-						return { id: playlistItem.track.id };
-					});
+			var snapshotId = $http({
+				url: API_ENDPOINT + "/users/" + userId + "/playlists/" + playlistId,
+				params: {
+					fields: "snapshot_id"
+				},
+				headers: getHeaders(authorization)
+			});
+			var tracks = loadAll({
+				url: API_ENDPOINT + "/users/" + userId + "/playlists/" + playlistId + "/tracks",
+				params: {
+					fields: ["next", "items.track.id", "items.track.uri"].join(","),
+					limit: 100
+				},
+				headers: getHeaders(authorization)
+			});
+
+			return $q.all([snapshotId, tracks])
+				.then(function(allData) {
+					return {
+						snapshotId: allData[0].data.snapshot_id,
+						items: allData[1]
+							.map(function(playlistItem) {
+								return {
+									id: playlistItem.track.id,
+									uri: playlistItem.track.uri
+								};
+							})
+					};
 				});
 		};
 	}
