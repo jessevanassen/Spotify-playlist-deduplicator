5561089e40f84c7c23b99194db9027e17ee76536
Jesse van Assen <jesse.v.assen@gmail.com>
2015-01-04 13:41:27 +0100

    Merge from master.

---
+++ scripts/appSettings.js
@@ -0,0 +1,8 @@
+(function() {
+	var appSettings = {
+		clientId: "368d5edf759e416c89bf60544223292c",
+		redirectUri: "http://JessevanAssen.github.io/Spotify-playlist-deduplicator/"
+	};
+
+	angular.module("spotify-playlist-deduplicator").constant("appSettings", appSettings);
+})();
